be2cd67a13398db9b1b52f81b6e2a11d3a96495d
Esteban Borai <estebanborai@gmail.com>
2023-05-13 22:39:05 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<div>
+  <h1 align="center">xemurs</h1>
+  <h4 align="center">Extended Emulators CLI</h4>
+</div>
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+const ABOUT: &str = r#"
+██╗  ██╗███████╗███╗   ███╗██╗   ██╗██████╗ ███████╗
+╚██╗██╔╝██╔════╝████╗ ████║██║   ██║██╔══██╗██╔════╝
+ ╚███╔╝ █████╗  ██╔████╔██║██║   ██║██████╔╝███████╗
+ ██╔██╗ ██╔══╝  ██║╚██╔╝██║██║   ██║██╔══██╗╚════██║
+██╔╝ ██╗███████╗██║ ╚═╝ ██║╚██████╔╝██║  ██║███████║
+╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝"#;
+
+#[derive(Parser, Debug)]
+#[command(bin_name = "xemurs")]
+#[command(next_line_help = true)]
+#[command(name = "xemurs", author, version, about, long_about = Some(ABOUT))]
+struct Cli;
+
+fn main() {
+    Cli::parse();
+}
