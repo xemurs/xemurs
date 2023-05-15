ea98a1485c852b3704fea2d897bff63612b5173f
Esteban Borai <estebanborai@gmail.com>
2023-05-14 23:23:33 -0400

    fix: use `SystemConfig`

--- crates/drivers/src/window.rs
+++ crates/drivers/src/window.rs
@@ -1,21 +1,15 @@
 use sdl2::Sdl;
 use sdl2::render::Canvas;
 
+use crate::SystemConfig;
 use crate::error::{Error, Result};
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct WindowConfig {
-  title: String,
-  height: u32,
-  width: u32,
-}
 
 pub struct Window {
     pub(crate) canvas: Canvas<sdl2::video::Window>,
 }
 
 impl Window {
-    pub fn new(ctx: &Sdl, config: WindowConfig) -> Result<Self> {
+    pub fn new(ctx: &Sdl, config: SystemConfig) -> Result<Self> {
         let video_subsystem = ctx.video().map_err(|err| {
           Error::VideoSubsystemInitialization(err)
         })?;
