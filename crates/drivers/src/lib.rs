7e4e1da938efa1023ab6e037484109e53853231b
Esteban Borai <estebanborai@gmail.com>
2023-05-14 23:22:24 -0400

    feat: provide `System` implementation

--- crates/drivers/src/lib.rs
+++ crates/drivers/src/lib.rs
@@ -1,5 +1,26 @@
 mod error;
-
-pub mod window;
+mod window;
 
 pub use error::{Error, Result};
+
+use self::window::{Window, WindowConfig};
+
+pub struct SystemConfig {
+    title: String,
+    height: u32,
+    width: u32,
+}
+
+pub struct System {
+    window: Window,
+}
+
+impl System {
+    pub fn new(config: SystemConfig) -> Result<Self> {
+        let window = Window::new(config)?;
+
+        Ok(Self {
+            window,
+        })
+    }
+}
